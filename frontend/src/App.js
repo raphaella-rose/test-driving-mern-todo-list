abda305937e281220e321e466885d9fea04588ce
raphaella-rose <raphaella.rose1@gmail.com>
2022-09-06 11:10:44 +0100

    form renders on page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,10 @@ function App() {
   return (
     <div className="App">
       <h1>Todo List</h1> 
+      <form data-testid="form-element">
+        <input type="text" placeholder="Enter a task..." data-testid="form-input" />
+        <input type="submit" value="Add" data-testid="form-submit" />
+      </form>
     </div>
   );
 }
--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -6,3 +6,13 @@ test('title is on screen', () => {
   const titleElement = screen.getByText("Todo List");
   expect(titleElement).toBeInTheDocument();
 });
+
+test('form is on screen', () => {
+  render(<App />);
+  const formElement = screen.getByTestId("form-element");
+  const taskInputElement = screen.getByTestId("form-input");
+  const taskSubmitElement = screen.getByTestId("form-submit");
+  expect(formElement).toBeInTheDocument();
+  expect(taskSubmitElement).toBeInTheDocument();
+  expect(taskInputElement).toBeInTheDocument();
+})
